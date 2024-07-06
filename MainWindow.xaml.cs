fa9b271ded7edc70744d8e74e6aa9e92c040dec9
Alienware <moycarlos09@gmail.com>
2024-07-06 13:03:12 -0400

    v1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayerUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        songList.ItemsSource = new[]
+
+        {
+
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Broken Mirror", ArtistName = "Hardwell"},
+
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Pacman", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Broken Mirror", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Broken Mirror", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Broken Mirror", ArtistName = "Hardwell"}
+
+
+        };
+        
+        songList2.ItemsSource = new[]
+
+        {
+
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Broken Mirror", ArtistName = "Hardwell"},
+
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"},
+            
+            new Song { ImageUrl = "C:\\Users\\Alienware\\RiderProjects\\MusicPlayerUI\\Images\\500x500-000000-80-0-0.jpg", SongName = "Rebels Never Die", ArtistName = "Hardwell"}
+            
+        };
+    }
+    
+   
+}
+
+public class Song
+
+{
+
+    public string ImageUrl { get; set; }
+
+   
+
+    public string SongName { get; set; }
+
+    public string ArtistName { get; set; }
+
+}
